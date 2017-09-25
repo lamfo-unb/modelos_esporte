24900ee178d2567d858058e89b99ed9a55794dea
IgorNascimento <igor.ferreira.n@gmail.com>
2017-09-25 20:14:46 -0300

    outros arquivos

--- program/transfermarket.R
+++ program/transfermarket.R
@@ -626,7 +626,7 @@ transfermarket <- function(temporadas_vec=0,jogos_vec=0,sobrescreve=T){
 
 #### Loop
 ## Funcao entre 2016 e 2011 "249"
-transfermarket(temporadas_vec = 2008,
+transfermarket(temporadas_vec = 2009,
                jogos_vec = 0, 
                sobrescreve = F)
 
